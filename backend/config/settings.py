a81d474affa45a242f4da58a705a4c875a95fe78
Shinyi Michael N. <buckxngmiccairo@gmail.com>
2026-09-07 11:55:40 +0100

    Configure Cloudinary media storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -231,7 +231,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 STORAGES = {
     "default": {
         "BACKEND": (
-            "django.core.files.storage.FileSystemStorage"
+            "cloudinary_storage.storage."
+            "MediaCloudinaryStorage"
         ),
     },
     "staticfiles": {
